a7ed1c43a1de729e17707e4986f7945d968af1a0
zochris <20927697+zochris@users.noreply.github.com>
2020-10-07 23:02:48 +0200

    Update exiled dependency

--- Spectator-Disabler/SpectatorDisabler.cs
+++ Spectator-Disabler/SpectatorDisabler.cs
@@ -15,7 +15,7 @@ namespace SpectatorDisabler
 
         public override string Name { get; } = "SpectatorDisabler";
         public override string Author { get; } = "zochris";
-        public override Version RequiredExiledVersion { get; } = new Version(2, 1, 0);
+        public override Version RequiredExiledVersion { get; } = new Version(2, 1, 7);
 
         public override void OnEnabled()
         {
