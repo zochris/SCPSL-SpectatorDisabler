63d382fc8575c330164c68936dc542c8221d3120
zochris <20927697+zochris@users.noreply.github.com>
2023-11-05 22:30:18 +0100

    Document transpilers properly

--- Spectator-Disabler/Patches/Scp049ResurrectCheckTargetPatch.cs
+++ Spectator-Disabler/Patches/Scp049ResurrectCheckTargetPatch.cs
@@ -2,18 +2,32 @@ using System.Collections.Generic;
 using System.Reflection.Emit;
 using Exiled.API.Features.Pools;
 using HarmonyLib;
+using JetBrains.Annotations;
 using PlayerRoles;
 using PlayerRoles.PlayableScps.Scp049;
 
 namespace SpectatorDisabler.Patches
 {
     [HarmonyPatch(typeof(Scp049ResurrectAbility), nameof(Scp049ResurrectAbility.ServerValidateAny))]
-    internal class Scp049ResurrectCheckTargetPatch
+    internal static class Scp049ResurrectCheckTargetPatch
     {
+        /// <summary>
+        ///     This transpiler replaces the following condition:
+        ///     <code>ownerHub.roleManager.CurrentRole is SpectatorRole</code>
+        ///     With this one:
+        ///     <code>ownerHub.roleManager.CurrentRole.RoleTypeId == RoleTypeId.Tutorial</code>
+        /// </summary>
+        /// <param name="instructions">
+        ///     The <see cref="CodeInstruction" />s of the original
+        ///     <see cref="Scp049ResurrectAbility.ServerValidateAny" /> method.
+        /// </param>
+        /// <returns>
+        ///     The new patched <see cref="CodeInstruction" />s of the <see cref="Scp049ResurrectAbility.ServerValidateAny" />
+        ///     method.
+        /// </returns>
+        [UsedImplicitly]
         private static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
         {
-            // Replace: … && ownerHub.roleManager.CurrentRole is SpectatorRole && …
-            // With: … && ownerHub.roleManager.CurrentRole.RoleTypeId == RoleTypeId.Tutorial && …
             var newInstructions = ListPool<CodeInstruction>.Pool.Get(instructions);
 
             for (var i = 0; i < newInstructions.Count; i++)
